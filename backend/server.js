7c5d1f2153e5e914be1b11f5f6a3b42b9d120c5e
Stephanie Zhu <slzzhu@gmail.com>
2020-10-22 23:19:40 -0700

    Create user model in backend

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,22 @@ const passport = require('passport');
 const passportLocal = require('passport-local').Strategy;
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
-const expressSession = require('express-session');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express()
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(express.json());
 app.use(cors());
+app.use(session({
+  secret: 'secretcode',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(cookieParser('secretcode'))
+
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, 
   () => console.log('Connected to db!'))
